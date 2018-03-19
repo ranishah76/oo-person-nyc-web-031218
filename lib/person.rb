5e0e95dfedbe7a6eaf5f0d0bdf6fc3ccc8ff4784
Rani Shah <rani76@gmail.com>
2018-03-19 14:04:38 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -75,7 +75,7 @@ class Person
   def call_friend(person)
     self.happiness += 3
     person.happiness += 3
-    "Hi #{person.name}! It's #{self.name}. How are you?"
+    "Hi #{person.name}! It's #{name}. How are you?"
 
     # happiness=(person.happiness)
     # happiness=(@happiness)
